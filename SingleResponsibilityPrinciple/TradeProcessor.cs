d8d6a1cb0cca0da9dab6e293a36e8f071fa0ddce
Dustin Kowal <dkowal@css.edu>
2018-11-01 11:09:41 -0500

    Request 407, Read the trades from a remote call to a web service

--- SingleResponsibilityPrinciple/TradeProcessor.cs
+++ SingleResponsibilityPrinciple/TradeProcessor.cs
@@ -148,7 +148,28 @@ namespace SingleResponsibilityPrinciple
             var trades = ParseTrades(lines);
             StoreTrades(trades);
         }
+        public void ProcessTrades(Stream stream, string url)
+        {
+            var lines = ReadURLTradeData(url);
+            var trades = ParseTrades(lines);
+            StoreTrades(trades);
+        }
 
-
+        public List<string> ReadURLTradeData(string URL)
+        {
+            var tradeData = new List<string>();
+            // create a web client and use it to read the file stored at the given URL
+            var client = new WebClient();
+            using (var stream = client.OpenRead(URL))
+            using (var reader = new StreamReader(stream))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    tradeData.Add(line);
+                }
+            }
+            return tradeData;
+        }
     }
 }
